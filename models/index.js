5f5bfef0851d46d123adc4b462e5140cc9f4749f
Marjorie <marjoriebaut.w@gmail.com>
2023-05-12 01:46:10 -0400

    a

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey:'product_id',
+through:{
+  model:'product_tag',
+  unique:false
+}
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+foreignKey:'product_id',
+through:{
+  model:'product_tag',
+  unique:false
+}
+})
 
 module.exports = {
   Product,
